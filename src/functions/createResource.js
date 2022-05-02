5ac0fab7cbebbca98c5c971dea8080f10925a89b
emilienmarion <emilien.marion@insa-lyon.fr>
2022-05-02 09:40:18 +0200

    create resource cloud

---
+++ src/functions/createResource.js
@@ -0,0 +1,38 @@
+Moralis.Cloud.define("createResource", async (request) => {
+    const logger = Moralis.Cloud.getLogger();
+  
+    const Resource = Moralis.Object.extend("Resource");
+    const resource = new Resource();
+  
+    resource.set("title", request.params.title);
+    if(request.params.link!=""){
+        resource.set("link",request.params.link);
+    }else if (request.params.markdown!=""){
+resource.set("markdown",request.params.markdown);
+    }
+
+    resource.set("format", request.params.format);
+    resource.set("liderbordId", request.params.liderbordId);
+  
+    await resource.save().then(
+      (resource) => {
+        // Execute any logic that should take place after the object is saved.
+        alert("New object created with objectId: " + resource.id);
+        return resource;
+      },
+      (error) => {
+        // Execute any logic that should take place if the save fails.
+        // error is a Moralis.Error with an error code and message.
+  
+        alert(
+          "Failed to create new resource, with error code: " + error.message
+        );
+        logger.error(
+          "createResource: Failed to create new resource, with error code: " +
+            error.message
+        );
+      }
+    );
+    return await resource.id;
+  });
+  
