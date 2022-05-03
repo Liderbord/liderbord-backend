e36781a091be06838c5bf346b4ba1a023455f422
Ewen Chaillan <echaillan@insa-lyon.fr>
2022-05-03 11:52:35 +0200

    search liderbord ok

--- src/functions/searchLiderbord.js
+++ src/functions/searchLiderbord.js
@@ -2,42 +2,23 @@ Moralis.Cloud.define("searchLiderbord", async (request) => {
     const logger = Moralis.Cloud.getLogger();
   
     const name = request.params.name;
-    logger.info(name);
+    
     const query = new Moralis.Query("Liderbord");
     query.startsWith("topic", name);
     const results = await query.find();
 
-    let liderbords = [];
+    let liderbordTab = [];
 
-    for (i=0; i<results.length; i++){
+    for(i=0;i<results.length;i++){
       let liderbord = {
-        topic: results[i].get("topic"),
-        id: results[i].get("objectId"),
-        description: results[i].get("description"),
-        tags: results[i].get("tags")
+        topic: results[0].get("topic"),
+        description: results[0].get("description"),
+        tags: results[0].get("tags"),
       }
-      liderbords.push(liderbord);
+      liderbordTab.push(liderbord);
     }
 
-  
-    await liderbord.save().then(
-      (liderbord) => {
-        // Execute any logic that should take place after the object is saved.
-        alert("Searching started...");
-        return liderbords;
-      },
-      (error) => {
-        // Execute any logic that should take place if the save fails.
-        // error is a Moralis.Error with an error code and message.
-  
-        alert(
-          "Failed to search liderbords, with error code: " + error.message
-        );
-        logger.error(
-          "searchLiderbord: Failed to search liderbords, with error code: " +
-            error.message
-        );
-      }
-    );
-    return await liderbords;
+
+
+    return liderbordTab;
   });
